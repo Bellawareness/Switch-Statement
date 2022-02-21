2884285ac7a5acfaf4ef344057d30abd4e2e8abd
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-02-21 22:39:34 +0000

    Initial commit

---
+++ Chef.java
@@ -0,0 +1,45 @@
+class Chef {
+  static void cookFood(int dish){
+   int x = 2;
+
+	switch(x) {
+
+
+
+
+	 case 1: System.out.println("Here are your fries");
+	break;
+
+ case 3:
+System.out.println("Here are your wings");
+break;
+
+  case 2: 
+System.out.println("Here is your steak");
+
+case 4:
+System.out.println("Here is your smoothie");
+
+default:
+ System.out.println("You want nothing?!");
+
+   case 8: 
+System.out.println("Here is your steak");
+break;
+
+case 5:
+System.out.println("Here is your salad");
+break;
+
+    // 1. Create a switch case with the following strings being printed out for each case.
+      // Here are your fries
+      // Here is your steak
+      // Here are your wings
+      // Here is your smoothie
+      // Here is your salad
+      // You want nothing?!
+
+
+  }
+  }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    // Make sure the chef gets the right dish!
+    // In the Chef.java file complete the cookFood method.
+
+    // This is the menu
+    System.out.println("1 for fries, 2 for a steak, 3 for wings, 4 for a smoothie, 5 for a salad, and any other number for nothing!");
+
+    // Customer orders food
+    Chef.cookFood(2);
+
+  }
+}
+
